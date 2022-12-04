8e97af1c301e655b7da46a9f6dd63e26756a1946
George-mFF4G <gm1362@fayoum.edu.eg>
2022-12-04 22:27:02 +0200

    lint update

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
+  appName: `Udagram`,
   apiHost: `http://georgeudagram-env.eba-rmfuprn3.us-east-1.elasticbeanstalk.com/api/v0`,
 };
 
