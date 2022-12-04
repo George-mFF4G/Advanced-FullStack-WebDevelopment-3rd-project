b3816395ae03e9a329dfb9e46c3e762d61ddf77c
George-mFF4G <gm1362@fayoum.edu.eg>
2022-12-05 00:05:01 +0200

    try

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: `Udagram`,
-  apiHost: `http://Georgeudagram-env.eba-rmfuprn3.us-east-1.elasticbeanstalk.com/api/v0`,
+  apiHost: `http://Georgeudagram.eba-rmfuprn3.us-east-1.elasticbeanstalk.com/api/v0`,
 };
 
 /*
