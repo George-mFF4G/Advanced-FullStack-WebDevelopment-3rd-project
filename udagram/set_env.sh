66ad61e68a1192035d1ec43bc97d600417d49942
George-mFF4G <gm1362@fayoum.edu.eg>
2022-12-04 22:08:05 +0200

    first commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,13 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-1.cxyku6tqond9.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=arn:aws:s3:::george-udagram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=http://georgeudagram-env.eba-rmfuprn3.us-east-1.elasticbeanstalk.com/
+export AWS_ACCESS_KEY_ID=AKIAXU6U2HOSF34Q37XE
+export AWS_SECRET_ACCESS_KEY=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
